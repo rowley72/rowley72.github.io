9548766972b336262132bb5002481a4425bacbb1
Briggite Rowley <row18002@byui.edu>
2019-06-14 22:56:02 -0500

    updates

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -1,9 +1,23 @@
-var tempF = 76;
-var speed  = 5;
+var tempF = document.getElementById("temp").innerHTML;
+alert(tempF);
+tempF=parseInt(tempF);
+
+var speed= document.getElementById("speed").innerHTML;
+alert(speed);
+speed= parseInt(speed);
+
+
+
+/*var tempF = 76;
+var speed  = 5;*/
 var t=tempF;
 var s=Math.pow(speed,0.16);
 windchilF=35.74+0.6215*t-35.75*s+0.4275*t*s;
 windchilFR=parseFloat(windchilF.toFixed(1));
 
-document.getElementById('windchill').innerHTML = windchilFR;
-    
+document.getElementById("windchill").innerHTML = windchilFR;
+
+
+
+
+
