fc82d5038a762ecea0c2923569528d4b7f165593
Briggite Rowley <row18002@byui.edu>
2019-06-21 19:45:27 -0500

    lesson-9 updates

---
+++ assignments/lesson-9/js/json-script.js
@@ -0,0 +1,48 @@
+var sections = document.querySelectorAll('section');
+var requestURL='https://byui-cit230.github.io/weather/data/towndata.json';
+
+var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+  var towndata = request.response;
+  datasection(towndata);
+  
+}
+function datasection(jsonObj) {
+    var infodata = jsonObj['towns'];
+    
+    for (var n = 0; n < sections.length; n++) {
+      for (var i = 0; i < infodata.length; i++) {
+        var idName = infodata[i].name.toLowerCase().replace(" ", "");
+        if (idName === sections[n].id) {
+          var myArticle = document.createElement('article');
+          var myH2 = document.createElement('h2');
+          var myPara1 = document.createElement('p');
+          var myPara2 = document.createElement('p');
+          var myPara3 = document.createElement('p');
+          var myPara4 = document.createElement('p');
+          var myImg = document.createElement("img");
+
+          myH2.textContent = infodata[i].name;
+          myPara1.textContent = 'motto: ' +infodata[i].motto;
+          myPara2.textContent = 'yearFounded: ' + infodata[i].yearFounded;
+          myPara3.textContent = 'currentPopulation:'+ infodata[i].currentPopulation;
+          myPara4.textContent = 'averageRainfall: ' +infodata[i].averageRainfall;
+          myImg.src = "images/" + idName + "-section.jpg";
+          myImg.className = "homeimage";
+          myArticle.appendChild(myH2);
+          myArticle.appendChild(myPara1);
+          myArticle.appendChild(myPara2);
+          myArticle.appendChild(myPara3);
+          myArticle.appendChild(myPara4);
+          myArticle.appendChild(myImg);
+    
+          sections[n].appendChild(myArticle);
+      
+        }
+      }
+    }
+}
+
