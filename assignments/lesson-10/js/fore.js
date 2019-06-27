f7794b3819080559a969ad3ee09fac0ff93054f4
Briggite Rowley <row18002@byui.edu>
2019-06-26 22:23:33 -0500

    updates

--- assignments/lesson-10/js/fore.js
+++ assignments/lesson-10/js/fore.js
@@ -10,7 +10,7 @@ foreRequest.onload =  function () {
 
 
     var icon="http://openweathermap.org/img/w/"+ foreData.list[4].weather[0].icon+".png";
-    var desc=foreData.weather[0].description;
+    var desc=foreData.list[4].description;
     document.getElementById('for-img').setAttribute('src',icon);
     document.getElementById('for-img').setAttribute('alt',desc);
 
