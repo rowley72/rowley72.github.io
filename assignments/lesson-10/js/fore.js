e04a4c14f042315718182ae8f15c570492be7737
Briggite Rowley <row18002@byui.edu>
2019-06-27 23:30:56 -0500

    updates

--- assignments/lesson-10/js/fore.js
+++ assignments/lesson-10/js/fore.js
@@ -1,3 +1,28 @@
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+
+var weatherTableRows=document.getElementById("weatherinfo").rows;
 var foreRequest = new XMLHttpRequest();
 var apiURLstring='https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=49bba8f65e06218978ae5717239ff8c0';
 foreRequest.open('Get',apiURLstring,true);
@@ -9,7 +34,7 @@ foreRequest.onload =  function () {
     for(i=0; i<foreData.list.length;i++){
         var forcast=foreData.list[i]
         var forcast_date=new Date(forcast.dt_txt)
-        if(forcast_date.getHours()===18&&forcast_date.getDay()===4){
+        if(forcast_date.getHours()===18){
             
 
 
